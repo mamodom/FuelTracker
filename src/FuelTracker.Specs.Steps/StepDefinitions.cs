5805def6444e131846b40a4463c0b78639739fce
Maximo Dominguez <mamodom@gmail.com>
2015-08-30 13:26:14 -0400

    Added step assembly

---
+++ src/FuelTracker.Specs.Steps/StepDefinitions.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace FuelTracker.Specs.Steps
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        [Given(@"I added the following fillup")]
+        public void GivenIAddedTheFollowingFillup(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I list my fillups")]
+        public void WhenIListMyFillups()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should see the following fillups")]
+        public void ThenIShouldSeeTheFollowingFillups(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

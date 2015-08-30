4f5f450109c21947a65b79cc4f89fc219b1a67d8
Maximo Dominguez <mamodom@gmail.com>
2015-08-30 19:03:45 -0400

    Adding global.asax and adding route registrations

---
+++ src/Web/FuelTracker.Web.Api/App_Start/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace FuelTracker.Web.Api
+{
+    public static class RouteConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultRoute",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+                );
+        }
+    }
+}
---
+++ src/Web/FuelTracker.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FuelTracker.Web.Api.Global" Language="C#" %>
